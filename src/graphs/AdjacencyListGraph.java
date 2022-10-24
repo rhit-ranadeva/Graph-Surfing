2f45986751c63da3a6a9bfb86ef27560bee78f56
ranadeva <ranadeva@rose-hulman.edu>
2022-10-23 21:53:14 -0400

    Got full points for M1, still need to clean up & comment

--- src/graphs/AdjacencyListGraph.java
+++ src/graphs/AdjacencyListGraph.java
@@ -1,122 +1,295 @@
 package graphs;
 
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
-public class AdjacencyListGraph<T> extends Graph<T> {
+public class AdjacencyListGraph<T> extends Graph<T>
+{
 	Map<T,Vertex> keyToVertex;
 	
-	private class Vertex {
+	int size;
+	int numEdges;
+	
+	private class Vertex
+	{
 		T key;
 		List<Vertex> successors;
 		List<Vertex> predecessors;
 		
-		Vertex(T key) {
+		Vertex(T key)
+		{
 			this.key = key;
 			this.successors = new ArrayList<Vertex>();
 			this.predecessors = new ArrayList<Vertex>();
 		}
 	}
 	
-	AdjacencyListGraph(Set<T> keys) {
+	AdjacencyListGraph(Set<T> keys)
+	{
+		this.numEdges = 0;
 		this.keyToVertex = new HashMap<T,Vertex>();
-		for (T key : keys) {
+		for (T key : keys) 
+		{
 			Vertex v = new Vertex(key);
 			this.keyToVertex.put(key, v);
+			this.size++;
 		}
 	}
 
 	@Override
-	public int size() {
+	public int size()
+	{
 		// TODO Auto-generated method stub
-		return 0;
+		return this.size;
 	}
 
 	@Override
-	public int numEdges() {
+	public int numEdges()
+	{
 		// TODO Auto-generated method stub
-		return 0;
+		return this.numEdges;
 	}
 
 	@Override
-	public boolean addEdge(T from, T to) {
+	public boolean addEdge(T from, T to) throws NoSuchElementException
+	{
 		// TODO Auto-generated method stub
-		return false;
+		if (!this.hasVertex(from) || !this.hasVertex(to))
+		{
+			throw new NoSuchElementException();
+		}
+		
+		Vertex fromVertex = this.keyToVertex.get(from);
+		Vertex toVertex = this.keyToVertex.get(to);
+		
+		if (fromVertex.successors.contains(toVertex))
+		{
+			return false;
+		}
+		
+		fromVertex.successors.add(toVertex);
+		toVertex.predecessors.add(fromVertex);
+		
+		this.numEdges++;
+		
+		return true;
 	}
 
 	@Override
-	public boolean hasVertex(T key) {
+	public boolean hasVertex(T key)
+	{
 		// TODO Auto-generated method stub
-		return false;
+		return this.keyToVertex.containsKey(key);
 	}
 
 	@Override
-	public boolean hasEdge(T from, T to) throws NoSuchElementException {
+	public boolean hasEdge(T from, T to) throws NoSuchElementException
+	{
 		// TODO Auto-generated method stub
-		return false;
+		if (!this.hasVertex(from) || !this.hasVertex(to))
+		{
+			throw new NoSuchElementException();
+		}
+		
+		Vertex fromVertex = this.keyToVertex.get(from);
+		Vertex toVertex = this.keyToVertex.get(to);
+		
+		return fromVertex.successors.contains(toVertex);
 	}
 
 	@Override
-	public boolean removeEdge(T from, T to) throws NoSuchElementException {
+	public boolean removeEdge(T from, T to) throws NoSuchElementException
+	{
 		// TODO Auto-generated method stub
-		return false;
+		if (!this.hasVertex(from) || !this.hasVertex(to))
+		{
+			throw new NoSuchElementException();
+		}
+		
+		Vertex fromVertex = this.keyToVertex.get(from);
+		Vertex toVertex = this.keyToVertex.get(to);
+		
+		if (!fromVertex.successors.contains(toVertex))
+		{
+			return false;
+		}
+		
+		fromVertex.successors.remove(toVertex);
+		toVertex.predecessors.remove(fromVertex);
+		
+		this.numEdges--;
+		
+		return true;
 	}
 
 	@Override
-	public int outDegree(T key) {
+	public int outDegree(T key) throws NoSuchElementException
+	{
 		// TODO Auto-generated method stub
-		return 0;
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		return this.keyToVertex.get(key).successors.size();
 	}
 
 	@Override
-	public int inDegree(T key) {
+	public int inDegree(T key) throws NoSuchElementException
+	{
 		// TODO Auto-generated method stub
-		return 0;
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		return this.keyToVertex.get(key).predecessors.size();
 	}
 
 	@Override
-	public Set<T> keySet() {
+	public Set<T> keySet()
+	{
+		// TODO Auto-generated method stub
+		return this.keyToVertex.keySet();
+	}
+
+	@Override
+	public Set<T> successorSet(T key) throws NoSuchElementException
+	{
+		// TODO Auto-generated method stub
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		Set<T> retSet = new HashSet<T>();
+		for (Vertex successor : this.keyToVertex.get(key).successors)
+		{
+			retSet.add(successor.key);
+		}
+		return retSet;
+	}
+
+	@Override
+	public Set<T> predecessorSet(T key) throws NoSuchElementException
+	{
+		// TODO Auto-generated method stub
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		Set<T> retSet = new HashSet<T>();
+		for (Vertex predecessor : this.keyToVertex.get(key).predecessors)
+		{
+			retSet.add(predecessor.key);
+		}
+		return retSet;
+	}
+
+	@Override
+	public Iterator<T> successorIterator(T key) throws NoSuchElementException
+	{
+		// TODO Auto-generated method stub
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		return new SuccessorIterator(key);
+	}
+	
+	class SuccessorIterator implements Iterator<T>
+	{
+		private int index;
+		private T key;
+		
+		public SuccessorIterator(T key)
+		{
+			this.key = key;
+			this.index = 0;
+		}
+		
+		@Override
+		public boolean hasNext() 
+		{
+			// By definition, we have a next item if curNode isn't null
+			return this.index < AdjacencyListGraph.this.keyToVertex.get(key).successors.size();
+		}
+		
+		@Override
+		public T next() throws NoSuchElementException, ConcurrentModificationException
+		{
+			// If there's no next item, throw NoSuchElementException
+			if (!hasNext())
+			{
+				throw new NoSuchElementException();
+			}
+			
+			this.index++;
+			
+			// Return the return value
+			return AdjacencyListGraph.this.keyToVertex.get(key).successors.get(index - 1).key;
+		}
+	}
+
+	@Override
+	public Iterator<T> predecessorIterator(T key) throws NoSuchElementException
+	{
+		// TODO Auto-generated method stub
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		return new PredecessorIterator(key);
+	}
+	
+	class PredecessorIterator implements Iterator<T>
+	{
+		private int index;
+		private T key;
+		
+		public PredecessorIterator(T key)
+		{
+			this.key = key;
+			this.index = 0;
+		}
+		
+		@Override
+		public boolean hasNext() 
+		{
+			// By definition, we have a next item if curNode isn't null
+			return this.index < AdjacencyListGraph.this.keyToVertex.get(key).predecessors.size();
+		}
+		
+		@Override
+		public T next() throws NoSuchElementException, ConcurrentModificationException
+		{
+			// If there's no next item, throw NoSuchElementException
+			if (!hasNext())
+			{
+				throw new NoSuchElementException();
+			}
+			
+			this.index++;
+			
+			// Return the return value
+			return AdjacencyListGraph.this.keyToVertex.get(key).predecessors.get(index - 1).key;
+		}
+	}
+
+	@Override
+	public Set<T> stronglyConnectedComponent(T key)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Set<T> successorSet(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<T> predecessorSet(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Iterator<T> successorIterator(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Iterator<T> predecessorIterator(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<T> stronglyConnectedComponent(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<T> shortestPath(T startLabel, T endLabel) {
+	public List<T> shortestPath(T startLabel, T endLabel)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/graphs/AdjacencyMatrixGraph.java
+++ src/graphs/AdjacencyMatrixGraph.java
@@ -1,112 +1,357 @@
 package graphs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
-public class AdjacencyMatrixGraph<T> extends Graph<T> {
+import graphs.AdjacencyListGraph.PredecessorIterator;
+import graphs.AdjacencyListGraph.SuccessorIterator;
+
+public class AdjacencyMatrixGraph<T> extends Graph<T>
+{
 	Map<T,Integer> keyToIndex;
 	List<T> indexToKey;
 	int[][] matrix;
 	
-	AdjacencyMatrixGraph(Set<T> keys) {
+	int size;
+	int numEdges;
+	
+	AdjacencyMatrixGraph(Set<T> keys)
+	{
 		int size = keys.size();
 		this.keyToIndex = new HashMap<T,Integer>();
 		this.indexToKey = new ArrayList<T>();
 		this.matrix = new int[size][size];
 		// need to populate keyToIndex and indexToKey with info from keys
+		this.size = size;
+		this.numEdges = 0;
+		int index = 0;
+		for (T key : keys)
+		{
+			keyToIndex.put(key, index);
+			indexToKey.add(key);
+			index++;
+		}
 	}
 	
 	@Override
-	public int size() {
+	public int size()
+	{
 		// TODO Auto-generated method stub
-		return 0;
+		return this.size;
 	}
 
 	@Override
-	public int numEdges() {
+	public int numEdges()
+	{
 		// TODO Auto-generated method stub
-		return 0;
+		return this.numEdges;
 	}
 
 	@Override
-	public boolean addEdge(T from, T to) {
+	public boolean addEdge(T from, T to)
+	{
 		// TODO Auto-generated method stub
-		return false;
+		if (!this.hasVertex(from) || !this.hasVertex(to))
+		{
+			throw new NoSuchElementException();
+		}
+		
+		int fromIndex = this.keyToIndex.get(from);
+		int toIndex = this.keyToIndex.get(to);
+		
+		if (this.matrix[fromIndex][toIndex] == 1)
+		{
+			return false;
+		}
+		
+		this.matrix[fromIndex][toIndex] = 1;
+		
+		this.numEdges++;
+		
+		return true;
 	}
 
 	@Override
-	public boolean hasVertex(T key) {
+	public boolean hasVertex(T key)
+	{
 		// TODO Auto-generated method stub
-		return false;
+		return this.keyToIndex.containsKey(key);
 	}
 
 	@Override
-	public boolean hasEdge(T from, T to) throws NoSuchElementException {
+	public boolean hasEdge(T from, T to) throws NoSuchElementException
+	{
 		// TODO Auto-generated method stub
-		return false;
+		if (!this.hasVertex(from) || !this.hasVertex(to))
+		{
+			throw new NoSuchElementException();
+		}
+		
+		int fromIndex = this.keyToIndex.get(from);
+		int toIndex = this.keyToIndex.get(to);
+		
+		return this.matrix[fromIndex][toIndex] == 1;
 	}
 
 	@Override
-	public boolean removeEdge(T from, T to) throws NoSuchElementException {
+	public boolean removeEdge(T from, T to) throws NoSuchElementException
+	{
 		// TODO Auto-generated method stub
-		return false;
+		if (!this.hasVertex(from) || !this.hasVertex(to))
+		{
+			throw new NoSuchElementException();
+		}
+		
+		int fromIndex = this.keyToIndex.get(from);
+		int toIndex = this.keyToIndex.get(to);
+		
+		if (this.matrix[fromIndex][toIndex] == 0)
+		{
+			return false;
+		}
+		
+		this.matrix[fromIndex][toIndex] = 0;
+		
+		this.numEdges--;
+		
+		return true;
 	}
 
 	@Override
-	public int outDegree(T key) {
+	public int outDegree(T key) throws NoSuchElementException
+	{
 		// TODO Auto-generated method stub
-		return 0;
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		
+		int row = this.keyToIndex.get(key);
+		int outDegree = 0;
+		
+		for (int col = 0; col < this.size(); col++)
+		{
+			if (this.matrix[row][col] == 1)
+			{
+				outDegree++;
+			}
+		}
+		
+		return outDegree;
 	}
 
 	@Override
-	public int inDegree(T key) {
+	public int inDegree(T key) throws NoSuchElementException
+	{
 		// TODO Auto-generated method stub
-		return 0;
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		
+		int col = this.keyToIndex.get(key);
+		int inDegree = 0;
+		
+		for (int row = 0; row < this.size(); row++)
+		{
+			if (this.matrix[row][col] == 1)
+			{
+				inDegree++;
+			}
+		}
+		
+		return inDegree;
 	}
 
 	@Override
-	public Set<T> keySet() {
+	public Set<T> keySet()
+	{
+		// TODO Auto-generated method stub
+		return new HashSet<T>(this.indexToKey);
+	}
+
+	@Override
+	public Set<T> successorSet(T key) throws NoSuchElementException
+	{
+		// TODO Auto-generated method stub
+		Set<T> retSet = new HashSet<T>();
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		int row = this.keyToIndex.get(key);
+		for (int col = 0; col < this.size(); col++)
+		{
+			if (this.matrix[row][col] == 1)
+			{
+				retSet.add(this.indexToKey.get(col));
+			}
+		}
+		return retSet;
+	}
+
+	@Override
+	public Set<T> predecessorSet(T key) throws NoSuchElementException
+	{
+		// TODO Auto-generated method stub
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		Set<T> retSet = new HashSet<T>();
+		int col = this.keyToIndex.get(key);
+		for (int row = 0; row < this.size(); row++)
+		{
+			if (this.matrix[row][col] == 1)
+			{
+				retSet.add(this.indexToKey.get(row));
+			}
+		}
+		return retSet;
+	}
+
+	@Override
+	public Iterator<T> successorIterator(T key) throws NoSuchElementException
+	{
+		// TODO Auto-generated method stub
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		return new SuccessorIterator(key);
+	}
+	
+	class SuccessorIterator implements Iterator<T>
+	{
+		private int index;
+		private T key;
+		
+		public SuccessorIterator(T key)
+		{
+			this.key = key;
+			this.index = 0;
+			if (matrix[keyToIndex.get(key)][this.index] == 0)
+			{
+				this.index++;
+				this.moveToNextValid();
+			}
+		}
+		
+		public void moveToNextValid()
+		{
+			while (this.index < size() && matrix[keyToIndex.get(key)][this.index] == 0)
+			{
+				this.index++;
+			}
+			return;
+		}
+		
+		@Override
+		public boolean hasNext() 
+		{
+			// By definition, we have a next item if curNode isn't null
+			return this.index < size();
+		}
+		
+		@Override
+		public T next() throws NoSuchElementException, ConcurrentModificationException
+		{
+			// If there's no next item, throw NoSuchElementException
+			if (!hasNext())
+			{
+				throw new NoSuchElementException();
+			}
+			
+			T retVal = indexToKey.get(index);
+			
+			this.index++;
+			this.moveToNextValid();
+			
+			// Return the return value
+			return retVal;
+		}
+	}
+
+	@Override
+	public Iterator<T> predecessorIterator(T key) throws NoSuchElementException
+	{
+		// TODO Auto-generated method stub
+		if (!this.hasVertex(key))
+		{
+			throw new NoSuchElementException();
+		}
+		return new PredecessorIterator(key);
+	}
+	
+	class PredecessorIterator implements Iterator<T>
+	{
+		private int index;
+		private T key;
+		
+		public PredecessorIterator(T key)
+		{
+			this.key = key;
+			this.index = 0;
+			if (matrix[this.index][keyToIndex.get(key)] == 0)
+			{
+				this.index++;
+				this.moveToNextValid();
+			}
+		}
+		
+		public void moveToNextValid()
+		{
+			while (this.index < size() && matrix[this.index][keyToIndex.get(key)] == 0)
+			{
+				this.index++;
+			}
+			return;
+		}
+		
+		@Override
+		public boolean hasNext() 
+		{
+			// By definition, we have a next item if curNode isn't null
+			return this.index < size();
+		}
+		
+		@Override
+		public T next() throws NoSuchElementException, ConcurrentModificationException
+		{
+			// If there's no next item, throw NoSuchElementException
+			if (!hasNext())
+			{
+				throw new NoSuchElementException();
+			}
+			
+			T retVal = indexToKey.get(index);
+			
+			this.index++;
+			this.moveToNextValid();
+			
+			// Return the return value
+			return retVal;
+		}
+	}
+
+	@Override
+	public Set<T> stronglyConnectedComponent(T key)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Set<T> successorSet(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<T> predecessorSet(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Iterator<T> successorIterator(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Iterator<T> predecessorIterator(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<T> stronglyConnectedComponent(T key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<T> shortestPath(T startLabel, T endLabel) {
+	public List<T> shortestPath(T startLabel, T endLabel)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
