eee5a949317091d7c60a3147dba49ef2a18e93d4
ranadeva <ranadeva@rose-hulman.edu>
2022-10-25 10:35:32 -0400

    ***MILESTONE 1 SUBMISSION***
    Commented & cleaned up everything

--- src/graphs/AdjacencyListGraph.java
+++ src/graphs/AdjacencyListGraph.java
@@ -10,35 +10,95 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
+/**
+ * Stores a Graph using adjacency lists
+ * 
+ * @author Vineet Ranade
+ *
+ * @param <T>
+ * 		Generic comparable type of Graph elements
+ */
 public class AdjacencyListGraph<T> extends Graph<T>
 {
+	/**
+	 * Maps keys to Vertex objects
+	 */
 	Map<T,Vertex> keyToVertex;
 	
+	/**
+	 * Number of vertices
+	 */
 	int size;
+	
+	/**
+	 * Number of edges
+	 */
 	int numEdges;
 	
+	/**
+	 * Class representing a Vertex in the Graph
+	 * 
+	 * @author Vineet Ranade
+	 *
+	 */
 	private class Vertex
 	{
+		/**
+		 * Key corresponding to this Vertex
+		 */
 		T key;
+		
+		/**
+		 * List of successors (outgoing edge)
+		 */
 		List<Vertex> successors;
+		
+		/**
+		 * List of predecessors (incoming edge)
+		 */
 		List<Vertex> predecessors;
 		
+		/**
+		 * Constructs a Vertex object
+		 * 
+		 * @param key
+		 * 		The key corresponding to this Vertex
+		 */
 		Vertex(T key)
 		{
+			// Initialize key using parameter
 			this.key = key;
+			
+			// Initialize successor and predecessor lists
 			this.successors = new ArrayList<Vertex>();
 			this.predecessors = new ArrayList<Vertex>();
 		}
 	}
 	
+	/**
+	 * Constructs an AdjacencyListGraph
+	 * 
+	 * @param keys
+	 * 		Set of keys for each Vertex in the Graph
+	 */
 	AdjacencyListGraph(Set<T> keys)
 	{
+		// Initialize numEdges to 0
 		this.numEdges = 0;
+		
+		// Initialize HashMap for mapping key to Vertex
 		this.keyToVertex = new HashMap<T,Vertex>();
+		
+		// For each key...
 		for (T key : keys) 
 		{
+			// Make a new Vertex from that key
 			Vertex v = new Vertex(key);
+			
+			// Add key-Vertex pair to HashMap
 			this.keyToVertex.put(key, v);
+			
+			// Increment size since we added a Vertex
 			this.size++;
 		}
 	}
@@ -47,6 +107,8 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public int size()
 	{
 		// TODO Auto-generated method stub
+		
+		// Use field
 		return this.size;
 	}
 
@@ -54,6 +116,8 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public int numEdges()
 	{
 		// TODO Auto-generated method stub
+		
+		// Use field
 		return this.numEdges;
 	}
 
@@ -61,22 +125,35 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public boolean addEdge(T from, T to) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Both Vertices must exist
 		if (!this.hasVertex(from) || !this.hasVertex(to))
 		{
 			throw new NoSuchElementException();
 		}
 		
+		// Obtain starting and ending Vertices
 		Vertex fromVertex = this.keyToVertex.get(from);
 		Vertex toVertex = this.keyToVertex.get(to);
 		
+		// If an edge already exists between the two...
+		// NOTE: checking predecessors of the ending Vertex is redundant!
 		if (fromVertex.successors.contains(toVertex))
 		{
+			// Return false...can't add the same edge
 			return false;
 		}
 		
+		// Otherwise, add the edge
+		
+		/*
+		 * Add the ending Vertex to successors of starting Vertex
+		 * Add the starting Vertex to predecessors of ending Vertex
+		 */
 		fromVertex.successors.add(toVertex);
 		toVertex.predecessors.add(fromVertex);
 		
+		// Increment numEdges field and return true
 		this.numEdges++;
 		
 		return true;
@@ -86,6 +163,8 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public boolean hasVertex(T key)
 	{
 		// TODO Auto-generated method stub
+		
+		// Check if the HashMap has this key
 		return this.keyToVertex.containsKey(key);
 	}
 
@@ -93,14 +172,18 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public boolean hasEdge(T from, T to) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Both Vertices must exist
 		if (!this.hasVertex(from) || !this.hasVertex(to))
 		{
 			throw new NoSuchElementException();
 		}
 		
+		// Obtain starting and ending Vertices
 		Vertex fromVertex = this.keyToVertex.get(from);
 		Vertex toVertex = this.keyToVertex.get(to);
 		
+		// Check if an edge exists between the two
 		return fromVertex.successors.contains(toVertex);
 	}
 
@@ -108,22 +191,32 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public boolean removeEdge(T from, T to) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Both Vertices must exist
 		if (!this.hasVertex(from) || !this.hasVertex(to))
 		{
 			throw new NoSuchElementException();
 		}
 		
+		// Obtain starting and ending Vertices
 		Vertex fromVertex = this.keyToVertex.get(from);
 		Vertex toVertex = this.keyToVertex.get(to);
 		
+		// If an edge doesn't exists between the two...
 		if (!fromVertex.successors.contains(toVertex))
 		{
+			// Return false, can't remove a nonexistent edge
 			return false;
 		}
 		
+		/*
+		 * Remove the ending Vertex from successors of starting Vertex
+		 * Remove the starting Vertex from predecessors of ending Vertex
+		 */
 		fromVertex.successors.remove(toVertex);
 		toVertex.predecessors.remove(fromVertex);
 		
+		// Decrement numEdges field and return true
 		this.numEdges--;
 		
 		return true;
@@ -133,10 +226,14 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public int outDegree(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Both Vertices must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
+		
+		// Return size of the Vertex's successors list
 		return this.keyToVertex.get(key).successors.size();
 	}
 
@@ -144,10 +241,14 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public int inDegree(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Vertex must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
+		
+		// Return size of the Vertex's predecessors list
 		return this.keyToVertex.get(key).predecessors.size();
 	}
 
@@ -155,6 +256,8 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public Set<T> keySet()
 	{
 		// TODO Auto-generated method stub
+		
+		// Simply convert keys into HashMap to a Set!
 		return this.keyToVertex.keySet();
 	}
 
@@ -162,15 +265,21 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public Set<T> successorSet(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Vertex must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
+		
+		// For each successor, add its key to the Set to return
 		Set<T> retSet = new HashSet<T>();
 		for (Vertex successor : this.keyToVertex.get(key).successors)
 		{
 			retSet.add(successor.key);
 		}
+		
+		// Return after all keys have been added
 		return retSet;
 	}
 
@@ -178,15 +287,21 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public Set<T> predecessorSet(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Vertex must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
+		
+		// For each predecessor, add its key to the Set to return
 		Set<T> retSet = new HashSet<T>();
 		for (Vertex predecessor : this.keyToVertex.get(key).predecessors)
 		{
 			retSet.add(predecessor.key);
 		}
+		
+		// Return after all keys have been added
 		return retSet;
 	}
 
@@ -194,29 +309,62 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public Iterator<T> successorIterator(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Vertex must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
-		return new SuccessorIterator(key);
+		
+		// Use helper class below
+		return new SuccessorOrPredecessorIterator(key, true);
 	}
 	
-	class SuccessorIterator implements Iterator<T>
+	/**
+	 * Class with a SuccessorOrPredecessorIterator
+	 * 
+	 * @author Vineet Ranade
+	 *
+	 */
+	class SuccessorOrPredecessorIterator implements Iterator<T>
 	{
+		/**
+		 * Current index in successor list
+		 */
 		private int index;
-		private T key;
 		
-		public SuccessorIterator(T key)
-		{
-			this.key = key;
+		/**
+		 * List of Vertices (successors/predecessors) we care about
+		 */
+		private List<Vertex> verticesOfInterest;
+		
+		/**
+		 * Constructs a PredecessorIterator
+		 * 
+		 * @param key
+		 * 		The key of the Vertex we want predecessors for
+		 */
+		public SuccessorOrPredecessorIterator(T key, boolean successors)
+		{		
+			// Start at index 0 of the List
 			this.index = 0;
+			
+			// Initialize predecessor list by accessing the right Vertex
+			if (successors)
+			{
+				this.verticesOfInterest = keyToVertex.get(key).successors;				
+			}
+			else
+			{
+				this.verticesOfInterest = keyToVertex.get(key).predecessors;				
+			}
 		}
 		
 		@Override
 		public boolean hasNext() 
 		{
-			// By definition, we have a next item if curNode isn't null
-			return this.index < AdjacencyListGraph.this.keyToVertex.get(key).successors.size();
+			// Make sure our index is in range for verticesOfInterest List
+			return this.index < this.verticesOfInterest.size();
 		}
 		
 		@Override
@@ -228,10 +376,9 @@ public class AdjacencyListGraph<T> extends Graph<T>
 				throw new NoSuchElementException();
 			}
 			
-			this.index++;
-			
-			// Return the return value
-			return AdjacencyListGraph.this.keyToVertex.get(key).successors.get(index - 1).key;
+			// Return the key at this index in verticesOfInterest
+			// Increment index afterwards as bookkeeping for subsequent next() call		
+			return this.verticesOfInterest.get(index++).key;
 		}
 	}
 
@@ -239,45 +386,15 @@ public class AdjacencyListGraph<T> extends Graph<T>
 	public Iterator<T> predecessorIterator(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Vertex must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
-		return new PredecessorIterator(key);
-	}
-	
-	class PredecessorIterator implements Iterator<T>
-	{
-		private int index;
-		private T key;
 		
-		public PredecessorIterator(T key)
-		{
-			this.key = key;
-			this.index = 0;
-		}
-		
-		@Override
-		public boolean hasNext() 
-		{
-			// By definition, we have a next item if curNode isn't null
-			return this.index < AdjacencyListGraph.this.keyToVertex.get(key).predecessors.size();
-		}
-		
-		@Override
-		public T next() throws NoSuchElementException, ConcurrentModificationException
-		{
-			// If there's no next item, throw NoSuchElementException
-			if (!hasNext())
-			{
-				throw new NoSuchElementException();
-			}
-			
-			this.index++;
-			
-			// Return the return value
-			return AdjacencyListGraph.this.keyToVertex.get(key).predecessors.get(index - 1).key;
-		}
+		// Use helper class above
+		return new SuccessorOrPredecessorIterator(key, false);
 	}
 
 	@Override
--- src/graphs/AdjacencyMatrixGraph.java
+++ src/graphs/AdjacencyMatrixGraph.java
@@ -1,7 +1,6 @@
 package graphs;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.ConcurrentModificationException;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -11,18 +10,47 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
-import graphs.AdjacencyListGraph.PredecessorIterator;
-import graphs.AdjacencyListGraph.SuccessorIterator;
-
+/**
+ * Stores a Graph using adjacency matrix
+ * 
+ * @author Vineet Ranade
+ *
+ * @param <T>
+ * 		Generic comparable type of Graph elements
+ */
 public class AdjacencyMatrixGraph<T> extends Graph<T>
 {
-	Map<T,Integer> keyToIndex;
-	List<T> indexToKey;
-	int[][] matrix;
+	/**
+	 * Maps keys to indices
+	 */
+	Map<T, Integer> keyToIndex;
 	
+	/**
+	 * Maps indices to keys
+	 */
+	List<T> indexToKey;
+	
+	/**
+	 * Adjacency matrix
+	 */
+	int[][] matrix;
+
+	/**
+	 * Number of vertices
+	 */
 	int size;
+	
+	/**
+	 * Number of edges
+	 */
 	int numEdges;
 	
+	/**
+	 * Constructs an AdjacencyMatrixGraph
+	 * 
+	 * @param keys
+	 * 		Set of keys for each Vertex in the Graph
+	 */
 	AdjacencyMatrixGraph(Set<T> keys)
 	{
 		int size = keys.size();
@@ -30,13 +58,25 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 		this.indexToKey = new ArrayList<T>();
 		this.matrix = new int[size][size];
 		// need to populate keyToIndex and indexToKey with info from keys
+		
+		// Initialize size to length of Set
 		this.size = size;
+		// Initialize numEdges to 0
 		this.numEdges = 0;
+		
+		// Declare and initialize array index to 0
 		int index = 0;
+		
+		// For each key...
 		for (T key : keys)
 		{
+			// Map the key to the index
 			keyToIndex.put(key, index);
+			
+			// Map the index to the key
 			indexToKey.add(key);
+			
+			// Increment index for next key
 			index++;
 		}
 	}
@@ -45,6 +85,8 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public int size()
 	{
 		// TODO Auto-generated method stub
+		
+		// Use field
 		return this.size;
 	}
 
@@ -52,6 +94,8 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public int numEdges()
 	{
 		// TODO Auto-generated method stub
+		
+		// Use field
 		return this.numEdges;
 	}
 
@@ -59,21 +103,30 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public boolean addEdge(T from, T to)
 	{
 		// TODO Auto-generated method stub
+		
+		// Both Vertices must exist
 		if (!this.hasVertex(from) || !this.hasVertex(to))
 		{
 			throw new NoSuchElementException();
 		}
 		
+		// Obtain starting and ending indices
 		int fromIndex = this.keyToIndex.get(from);
 		int toIndex = this.keyToIndex.get(to);
 		
+		// If an edge already exists between the two...
 		if (this.matrix[fromIndex][toIndex] == 1)
 		{
+			// Return false...can't add the same edge
 			return false;
 		}
 		
+		// Otherwise add the edge
+		
+		// Set matrix value at [fromIndex][toIndex] to 1 to represent an edge
 		this.matrix[fromIndex][toIndex] = 1;
 		
+		// Increment numEdges field and return true		
 		this.numEdges++;
 		
 		return true;
@@ -83,6 +136,8 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public boolean hasVertex(T key)
 	{
 		// TODO Auto-generated method stub
+		
+		// Check if the keyToIndex mapping has this key
 		return this.keyToIndex.containsKey(key);
 	}
 
@@ -90,14 +145,18 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public boolean hasEdge(T from, T to) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Both Vertices must exist
 		if (!this.hasVertex(from) || !this.hasVertex(to))
 		{
 			throw new NoSuchElementException();
 		}
 		
+		// Obtain starting and ending indices		
 		int fromIndex = this.keyToIndex.get(from);
 		int toIndex = this.keyToIndex.get(to);
 		
+		// Check if an edge exists between the two
 		return this.matrix[fromIndex][toIndex] == 1;
 	}
 
@@ -105,21 +164,28 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public boolean removeEdge(T from, T to) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Both Vertices must exist
 		if (!this.hasVertex(from) || !this.hasVertex(to))
 		{
 			throw new NoSuchElementException();
 		}
 		
+		// Obtain starting and ending indices
 		int fromIndex = this.keyToIndex.get(from);
 		int toIndex = this.keyToIndex.get(to);
 		
+		// If an edge doesn't exists between the two...
 		if (this.matrix[fromIndex][toIndex] == 0)
 		{
+			// Return false, can't remove a nonexistent edge
 			return false;
 		}
 		
+		// Set matrix value at [fromIndex][toIndex] to 0 to represent no edge
 		this.matrix[fromIndex][toIndex] = 0;
 		
+		// Decrement numEdges field and return true
 		this.numEdges--;
 		
 		return true;
@@ -129,22 +195,30 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public int outDegree(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Both Vertices must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
 		
+		// fromIndex = row. Map key to row index
 		int row = this.keyToIndex.get(key);
+		
+		// Counter
 		int outDegree = 0;
 		
+		// For each column/col/toIndex in this row
 		for (int col = 0; col < this.size(); col++)
 		{
+			// Increment outDegree if an edge exists
 			if (this.matrix[row][col] == 1)
 			{
 				outDegree++;
 			}
 		}
 		
+		// Return final counter
 		return outDegree;
 	}
 
@@ -152,22 +226,30 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public int inDegree(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Both Vertices must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
 		
+		// toIndex = column/col. Map key to column index
 		int col = this.keyToIndex.get(key);
+		
+		// Counter
 		int inDegree = 0;
 		
+		// For each row/fromIndex in this row
 		for (int row = 0; row < this.size(); row++)
 		{
+			// Increment outDegree if an edge exists
 			if (this.matrix[row][col] == 1)
 			{
 				inDegree++;
 			}
 		}
 		
+		// Return final counter
 		return inDegree;
 	}
 
@@ -175,6 +257,8 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public Set<T> keySet()
 	{
 		// TODO Auto-generated method stub
+		
+		// Simply convert keys into HashMap to a Set!
 		return new HashSet<T>(this.indexToKey);
 	}
 
@@ -182,11 +266,17 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public Set<T> successorSet(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
-		Set<T> retSet = new HashSet<T>();
+		
+		// Vertex must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
+		
+		// For each toIndex with an edge, add its key to the Set to return
+		Set<T> retSet = new HashSet<T>();
+		
+		// The row/fromIndex is fixed since we want successors
 		int row = this.keyToIndex.get(key);
 		for (int col = 0; col < this.size(); col++)
 		{
@@ -195,6 +285,8 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 				retSet.add(this.indexToKey.get(col));
 			}
 		}
+		
+		// Return after all keys have been added
 		return retSet;
 	}
 
@@ -202,11 +294,17 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public Set<T> predecessorSet(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Vertex must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
+		
+		// For each fromIndex with an edge, add its key to the Set to return
 		Set<T> retSet = new HashSet<T>();
+		
+		// The column/col/toIndex is fixed since we want predecessors
 		int col = this.keyToIndex.get(key);
 		for (int row = 0; row < this.size(); row++)
 		{
@@ -215,6 +313,8 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 				retSet.add(this.indexToKey.get(row));
 			}
 		}
+		
+		// Return after all keys have been added
 		return retSet;
 	}
 
@@ -222,42 +322,106 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public Iterator<T> successorIterator(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Vertex must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
-		return new SuccessorIterator(key);
+		
+		// Use helper class below
+		return new SuccessorOrPredecessorIterator(key, true);
 	}
 	
-	class SuccessorIterator implements Iterator<T>
+	/**
+	 * Class with a SuccessorOrPredecessorIterator
+	 * 
+	 * @author Vineet Ranade
+	 *
+	 */
+	class SuccessorOrPredecessorIterator implements Iterator<T>
 	{
+		/**
+		 * Current index in successor list (column)
+		 */
 		private int index;
-		private T key;
 		
-		public SuccessorIterator(T key)
+		/**
+		 * Fixed index of the Vertex we want successors/predecessors for
+		 */
+		private int fixedIndex;
+		
+		/**
+		 * Whether we want successors or predecessors
+		 */
+		private boolean successors;
+		
+		/**
+		 * Constructs a SuccessorIterator
+		 * 
+		 * @param key
+		 * 		The key of the Vertex we want successors for
+		 */
+		public SuccessorOrPredecessorIterator(T key, boolean successors)
 		{
-			this.key = key;
+			this.successors = successors;
+			
+			// Fix the row at the right index
+			this.fixedIndex = keyToIndex.get(key);
+			
+			// Start at index 0 of the row
 			this.index = 0;
-			if (matrix[keyToIndex.get(key)][this.index] == 0)
+			
+			// If no edge exists at [row][0]...
+			if (this.noEdge())
 			{
+				// Move to the next index in matrix with an edge!
 				this.index++;
 				this.moveToNextValid();
 			}
 		}
 		
+		/**
+		 * Checks if no edge exists at the current position in matrix
+		 * 
+		 * @return
+		 * 		true if no edge exists
+		 * 		false if an edge exists
+		 */
+		public boolean noEdge()
+		{
+			// If we want successors, the fixedIndex is the row
+			if (this.successors)
+			{
+				return matrix[fixedIndex][this.index] == 0;
+			}
+			
+			// If we want predecessors, the fixedIndex is the column
+			else
+			{
+				return matrix[this.index][fixedIndex] == 0;
+			}
+		}
+		
+		/**
+		 * Helper method to move to next position in matrix with an edge
+		 */
 		public void moveToNextValid()
 		{
-			while (this.index < size() && matrix[keyToIndex.get(key)][this.index] == 0)
+			// While we're in range AND no edge exists currently
+			while (this.index < size() && this.noEdge())
 			{
+				// Move to the next position and try again
 				this.index++;
 			}
-			return;
+			
+			// Either we're out of range or have found an edge
 		}
 		
 		@Override
 		public boolean hasNext() 
 		{
-			// By definition, we have a next item if curNode isn't null
+			// Make sure our index is in range for successor indices
 			return this.index < size();
 		}
 		
@@ -270,8 +434,10 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 				throw new NoSuchElementException();
 			}
 			
+			// Obtain the key of the correct Vertex
 			T retVal = indexToKey.get(index);
 			
+			// Do bookkeeping for subsequent next() call
 			this.index++;
 			this.moveToNextValid();
 			
@@ -284,62 +450,15 @@ public class AdjacencyMatrixGraph<T> extends Graph<T>
 	public Iterator<T> predecessorIterator(T key) throws NoSuchElementException
 	{
 		// TODO Auto-generated method stub
+		
+		// Vertex must exist
 		if (!this.hasVertex(key))
 		{
 			throw new NoSuchElementException();
 		}
-		return new PredecessorIterator(key);
-	}
-	
-	class PredecessorIterator implements Iterator<T>
-	{
-		private int index;
-		private T key;
 		
-		public PredecessorIterator(T key)
-		{
-			this.key = key;
-			this.index = 0;
-			if (matrix[this.index][keyToIndex.get(key)] == 0)
-			{
-				this.index++;
-				this.moveToNextValid();
-			}
-		}
-		
-		public void moveToNextValid()
-		{
-			while (this.index < size() && matrix[this.index][keyToIndex.get(key)] == 0)
-			{
-				this.index++;
-			}
-			return;
-		}
-		
-		@Override
-		public boolean hasNext() 
-		{
-			// By definition, we have a next item if curNode isn't null
-			return this.index < size();
-		}
-		
-		@Override
-		public T next() throws NoSuchElementException, ConcurrentModificationException
-		{
-			// If there's no next item, throw NoSuchElementException
-			if (!hasNext())
-			{
-				throw new NoSuchElementException();
-			}
-			
-			T retVal = indexToKey.get(index);
-			
-			this.index++;
-			this.moveToNextValid();
-			
-			// Return the return value
-			return retVal;
-		}
+		// Use helper class above
+		return new SuccessorOrPredecessorIterator(key, false);
 	}
 
 	@Override
